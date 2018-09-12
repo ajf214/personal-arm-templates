666c2da088f3e6c9eae7443ff6e9e64a2a922135
ajf214 <alex.j.frankel@gmail.com>
2018-09-11 23:49:59 -0700

    changes related to v0.7 of contoso global blueprint

--- Contoso-Global-Blueprint/clean-subscription.ps1
+++ Contoso-Global-Blueprint/clean-subscription.ps1
@@ -11,11 +11,17 @@ Remove-AzureRmResourceGroup -Name "Security-01" -Verbose -Force
 Remove-AzureRmResourceGroup -Name "Management-01" -Verbose -Force
 Remove-AzureRmResourceGroup -Name "Application-01" -Verbose -Force
 
-# delete the role assignments
-
+# DELETE ROLE ASSIGNMENTS
 # remove Contoso IT
-Remove-AzureRmRoleAssignment -ObjectId "0605ea6f-1fc7-4067-bdc9-a0694a6c17b5" -RoleDefinitionName "Owner" -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641"
+Remove-AzureRmRoleAssignment -ObjectId "0605ea6f-1fc7-4067-bdc9-a0694a6c17b5" -RoleDefinitionName "Owner" -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641" -Verbose
 
-# remove Policy Assignments at sub
-Remove-AzureRmPolicyAssignment -Name "" -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641"
-Remove-AzureRmPolicyAssignment -Name "" -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641"
+# REMOVE POLICY ASSIGNMENTS AT SUB
+# get all policies assigned at this sub
+$policies = Get-AzureRmPolicyAssignment
+
+# loop through returned policies and remove them
+$policies | foreach {if($_.ResourceType -eq 'Microsoft.Authorization/policyAssignments') {
+        Write-Host 'Removing' $_.ResourceName
+        Remove-AzureRmPolicyAssignment -Name $_.ResourceName -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641"
+    }
+}

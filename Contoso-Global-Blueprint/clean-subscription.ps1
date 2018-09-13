1c21e8ba9de683861be95f9576e51fe3d21a25f0
ajf214 <alex.j.frankel@gmail.com>
2018-09-12 17:23:17 -0700

    up to 0.12, successfully doing multi-assign blueprint

--- Contoso-Global-Blueprint/clean-subscription.ps1
+++ Contoso-Global-Blueprint/clean-subscription.ps1
@@ -1,19 +1,25 @@
+# SUBSCRIPTIONS
+# Contoso IT - Retail - Prod: 45f9252d-e27e-4ed8-ab4e-dc5054de13fa
+# Contoso IT - Retail - Pre-Prod: 210b13f9-e96d-493c-919e-34e12038a338 
+# Contoso IT - Retail - DevTest: 35ad74f4-0b37-44a7-ba94-91b6ec6026cd
+
 # login to azure
 Connect-AzureRmAccount
+$subscriptionId = "35ad74f4-0b37-44a7-ba94-91b6ec6026cd"
 
 # set context to Contoso Infra1
 # will need to repeat this 3 times when there are 3 subscriptions
-Set-AzureRmContext -SubscriptionId "0ba674a6-9fde-43b4-8370-a7e16fdf0641"
+Set-AzureRmContext -SubscriptionId $subscriptionId
 
 # delete all four resource groups
-Remove-AzureRmResourceGroup -Name "Networking-01" -Verbose -Force
-Remove-AzureRmResourceGroup -Name "Security-01" -Verbose -Force
-Remove-AzureRmResourceGroup -Name "Management-01" -Verbose -Force
-Remove-AzureRmResourceGroup -Name "Application-01" -Verbose -Force
+Remove-AzureRmResourceGroup -Name "Networking-resource-group" -Verbose -Force
+Remove-AzureRmResourceGroup -Name "Security-resource-group" -Verbose -Force
+Remove-AzureRmResourceGroup -Name "Management-resource-group" -Verbose -Force
+Remove-AzureRmResourceGroup -Name "Application-resource-group" -Verbose -Force
 
 # DELETE ROLE ASSIGNMENTS
 # remove Contoso IT
-Remove-AzureRmRoleAssignment -ObjectId "0605ea6f-1fc7-4067-bdc9-a0694a6c17b5" -RoleDefinitionName "Owner" -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641" -Verbose
+Remove-AzureRmRoleAssignment -ObjectId "0605ea6f-1fc7-4067-bdc9-a0694a6c17b5" -RoleDefinitionName "Owner" -Scope "/subscriptions/$subscriptionId" -Verbose
 
 # REMOVE POLICY ASSIGNMENTS AT SUB
 # get all policies assigned at this sub
@@ -22,6 +28,6 @@ $policies = Get-AzureRmPolicyAssignment
 # loop through returned policies and remove them
 $policies | foreach {if($_.ResourceType -eq 'Microsoft.Authorization/policyAssignments') {
         Write-Host 'Removing' $_.ResourceName
-        Remove-AzureRmPolicyAssignment -Name $_.ResourceName -Scope "/subscriptions/0ba674a6-9fde-43b4-8370-a7e16fdf0641"
+        Remove-AzureRmPolicyAssignment -Name $_.ResourceName -Scope "/subscriptions/$subscriptionId"
     }
 }

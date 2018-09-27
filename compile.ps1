02012849aaf582d328fe2e0786cc6c6e18239810
ajf214 <alex.j.frankel@gmail.com>
2018-09-27 11:56:55 -0700

    added -alt for sake of demo

--- compile.ps1
+++ compile.ps1
@@ -16,7 +16,6 @@ function GenerateArmClientCommand([string] $fileName)
     $resourceId = $json.id;
 
     # first param is the URL for the PUT, second param is the body of the request, contained in the file
-    # there's some weird loop-ish type stuff going on - don't know how to describe it...
     return "armclient PUT $($resourceId)?api-version=2017-11-11-preview @$fileName";
 }
 

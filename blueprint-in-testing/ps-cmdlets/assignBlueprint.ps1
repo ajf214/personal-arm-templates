6bc006311ae6434754e4f765313fabce470185db
ajf214 <alex.j.frankel@gmail.com>
2019-03-10 22:43:00 -0700

    updated variables

--- blueprint-in-testing/ps-cmdlets/assignBlueprint.ps1
+++ blueprint-in-testing/ps-cmdlets/assignBlueprint.ps1
@@ -1,4 +1,7 @@
-$myBluerpint = Get-AzBlueprint -ManagementGroupId ContosoRoot -Name "deep-dive-01" -Version "1.4"
+$blueprintName = "deep-dive-01"
+$blueprintVersion = "1.4"
+
+$myBluerpint = Get-AzBlueprint -ManagementGroupId ContosoRoot -Name $blueprintName -Version $blueprintVersion
 $subId = "35ad74f4-0b37-44a7-ba94-91b6ec6026cd"
 
 $rgHash = @{ name="my-ps-rg"; location = "eastus" }
